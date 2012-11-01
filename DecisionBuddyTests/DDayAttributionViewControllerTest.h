a2b7dbf6a91ef62650522461a878f423d5cf1449
Elango Balusamy <elango@gmail.com>
2012-11-01 23:34:38 +0530

    project files

---
+++ DecisionBuddyTests/DDayAttributionViewControllerTest.h
@@ -0,0 +1,14 @@
+//
+//  DDayAttributionViewControllerTest.h
+//  DecisionBuddy
+//
+//  Created by Elango Balusamy on 10/3/12.
+//  Copyright (c) 2012 Elango Balusamy. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "DDayAttributionViewController.h"
+
+@interface DDayAttributionViewControllerTest : SenTestCase
+
+@end
---
+++ DecisionBuddyTests/DDayAttributionViewControllerTest.m
@@ -0,0 +1,20 @@
+//
+//  DDayAttributionViewControllerTest.m
+//  DecisionBuddy
+//
+//  Created by Elango Balusamy on 10/3/12.
+//  Copyright (c) 2012 Elango Balusamy. All rights reserved.
+//
+
+#import "DDayAttributionViewControllerTest.h"
+
+@implementation DDayAttributionViewControllerTest
+
+-(void)testDecisionPrefixForYes
+{
+    [[DDayAttributionViewController alloc] init];
+    STAssertEquals(1, 1, @"testing simple");
+    
+}
+
+@end
